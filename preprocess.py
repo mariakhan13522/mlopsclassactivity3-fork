52b3468c3e31957a6150b7a98a42d18844fdc9fa
mariakhan13522 <mariakhan13522@gmail.com>
2025-11-12 21:21:04 +0500

    Update preprocess.py
    creating data directory

--- preprocess.py
+++ preprocess.py
@@ -5,5 +5,8 @@ iris = load_iris()
 data = pd.DataFrame(iris.data, columns=iris.feature_names)
 data['target'] = iris.target
 
+# Create data directory
+os.makedirs("data", exist_ok=True)
+
 data.to_csv("data/preprocessed.csv", index=False)
 print("✅ Data preprocessed and saved to data/preprocessed.csv")
